c6f6adbf9c8b8ac137301e72e467a7bf3a4a7185
BarryZhuang <1007koi@gmail.com>
2025-07-04 20:26:36 +0800

    feat: GameView.vue加入使用CompueterPlayer組件
    - GameView.vue引入CompueterPlayer組件
    
    - 依照回合抓取組件ref進行出牌與發牌

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,42 +1,70 @@
 <template>
     <div class="container-fluid min-vh-100 p-0 bg-success">
-        <div class="d-flex justify-content-center align-items-start pt-2">
-            <h1 class="text-white">Card 99 Game</h1>
-        </div>
-        <div class="row d-flex justify-content-center mx-0 overflow-hidden">
-            <div class="col-4 p-0">
-                <ScorePanel ref="scorePanelRef" :latestPlayedCard="latestPlayedCard"
-                    @score-updated="gameScore = $event" />
+        <div class="row d-flex justify-content-center mx-0 ">
+            <div class="col-2 p-0">
+                <ComputerPlayer :isActive="activeIndex === otherPlayer.idx" :playerInfo="otherPlayer"
+                    :playerList="playerList" ref="computerPlayerRef" @playCard="handleCardScoring" />
+            </div>
+            <div class="col-10 p-0">
+                <div class="d-flex justify-content-center align-items-start pt-2">
+                    <h3 class="text-white">Card 99 Game</h3>
+                </div>
+                <div class="row d-flex justify-content-center mx-0 overflow-hidden">
+                    <div class="col-6 p-0">
+                        <ScorePanel ref="scorePanelRef" :latestPlayedCard="latestPlayedCard"
+                            @score-updated="gameScore = $event" />
+                    </div>
+                </div>
+                <PlayerArea ref="playerRef" :playerInfo="player" :playerList="playerList"
+                    :isActive="activeIndex === player.idx" @playCard="handleCardScoring" />
             </div>
         </div>
-        <PlayerArea ref="PlayerRef" :playerList="playerList" @playCard="handleCardScoring" />
     </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
 import type { CardValue, Card, Account } from '@/types/baseType'
 import { convertToCard } from '@/utils/cardUtils'
 import ScorePanel from '@/components/ScorePanel.vue'
 import PlayerArea from '@/components/Player.vue'
-let gameOver = ref(true);
+import ComputerPlayer from '@/components/ComputerPlayer.vue'
 let deckID = ref(null);
 const latestPlayedCard = ref<Card | null>(null);
 let gameScore = ref(0);
 const scorePanelRef = ref()
+let otherPlayer = ref<Account>(
+    { idx: 1, avatar: '', accountId: 'player1', name: 'Player1_Name' },
+)
+let player = ref<Account>(
+    { idx: 0, avatar: '', accountId: 'player0', name: 'BarryZhuang' }
+)
 let playerList: Account[] = []
 const gamePlayerList: Account[] = [
-    { avatar: '', accountId: 'player0', name: 'BarryZhuang' },
-    { avatar: '', accountId: 'player1', name: 'Player1_Name' },
-    { avatar: '', accountId: 'player2', name: 'Player2_Name' },
-    { avatar: '', accountId: 'player3', name: 'Player3_Name' }
+    { idx: 0, avatar: '', accountId: 'player0', name: 'BarryZhuang' },
+    { idx: 1, avatar: '', accountId: 'player1', name: 'Player1_Name' }
 ]
-const PlayerRef = ref()
+const computerPlayerRef = ref<InstanceType<typeof ComputerPlayer> | null>(null)
+const playerRef = ref<InstanceType<typeof PlayerArea> | null>(null)
+const activeIndex = ref(0)
+const isReversed = ref(false)
+const allPlayerRefs = computed(() => [
+    playerRef.value,
+    computerPlayerRef.value,
+])
+
+// 處理誰是下一位玩家
+const nextPlayer = () => {
+    if (isReversed.value) {
+        activeIndex.value = (activeIndex.value - 1 + allPlayerRefs.value.length) % allPlayerRefs.value.length
+    } else {
+        activeIndex.value = (activeIndex.value + 1) % allPlayerRefs.value.length
+    }
+}
 
 // 洗牌
 async function getDeck() {
     try {
-        gameOver.value = false;
         if (deckID.value == null) {
             const result = await axios.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
             deckID.value = result.data.deck_id;
@@ -49,32 +77,40 @@ async function getDeck() {
 // 一開始抽五張牌
 async function getInitCards() {
     try {
-        const result = await axios.get("https://www.deckofcardsapi.com/api/deck/" + deckID.value + "/draw/?count=5");
-        const resultCards = result.data.cards;
-        const initcardList: Card[] = []
-        resultCards.forEach((cv: CardValue) => {
-            initcardList.push(convertToCard(cv))
+        // 給每一位玩家發牌
+        allPlayerRefs.value.forEach(async child => {
+            const result = await axios.get(
+                `https://www.deckofcardsapi.com/api/deck/${deckID.value}/draw/?count=5`
+            )
+            const initCardList: Card[] = (result.data.cards as CardValue[])
+                .map(cv => convertToCard(cv))
+            child?.receiveCards([...initCardList])
         })
-        PlayerRef.value.receiveCards(initcardList)
     } catch (error) {
-        console.error('getCards error:', error);
+        console.error('getInitCards error:', error)
     }
 }
 
 // 將卡牌傳入ScorePanel 計分
 function handleCardScoring(card: Card) {
-    latestPlayedCard.value = card;
-    scorePanelRef.value?.handleScore(card);
-    getNewCard(PlayerRef)
+    latestPlayedCard.value = card
+    scorePanelRef.value?.handleScore(card)
+    //抓下一個玩家的組件ref
+    const nextInst = allPlayerRefs.value[activeIndex.value]
+    if (nextInst) {
+        getNewCard(nextInst)
+    }
+    nextPlayer()
 }
 
 // 發新一張牌到手牌
-async function getNewCard(refTarget: any) {
-    const result = await axios.get(
+async function getNewCard(targetInstance: { receiveCards: (cards: Card[]) => void }) {
+    console.log('getNewCard-targetInstance:', targetInstance)
+    const { data } = await axios.get(
         `https://www.deckofcardsapi.com/api/deck/${deckID.value}/draw/?count=1`
     )
-    const newCard = convertToCard(result.data.cards[0]);
-    refTarget.value.receiveCards([newCard]);
+    const newCard = convertToCard(data.cards[0] as CardValue)
+    targetInstance.receiveCards([newCard])
 }
 
 // 把使用者加入遊戲順序清單
