8a284242ac545ddbfd84a750d2d8f79bc44c6d85
BarryZhuang <1007koi@gmail.com>
2025-07-06 12:51:35 +0800

    feat: 新增sweetalert2、vue3-toastify套件

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.vue'
 import router from './router'
+import 'sweetalert2/dist/sweetalert2.min.css'
+import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App)
 
