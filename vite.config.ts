bbdaef1178b1327fab50c6b663c07dcc939b996e
BarryZhuang <1007koi@gmail.com>
2025-07-11 00:49:54 +0800

    ci: add GH Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Card-99-Game/',
   plugins: [
     vue(),
     vueDevTools(),
